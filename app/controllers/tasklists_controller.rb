24e540a50cd0dd442003f30bbdbeddec6ac8f5df
tomoyaaida <jwaptmwt15@gmail.com>
2017-04-26 03:34:37 +0000

    init Rails Project

--- app/controllers/tasklists_controller.rb
+++ app/controllers/tasklists_controller.rb
@@ -23,32 +23,32 @@ class TasklistsController < ApplicationController
   end
  end
 
-  def edit
-    @tasklist = Tasklist.find(params[:id])
-  end
+ def edit
+  @tasklist = Tasklist.find(params[:id])
+ end
 
-  def update
-    @tasklist = Tasklist.find(params[:id])
+ def update
+  @tasklist = Tasklist.find(params[:id])
 
-    if @tasklist.update(tasklist_params)
-      flash[:success] = 'Tasklist は正常に更新されました'
-      redirect_to @tasklist
-    else
-      flash.now[:danger] = 'Tasklist は更新されませんでした'
-      render :edit
-    end
-  end
+   if @tasklist.update(tasklist_params)
+    flash[:success] = 'Tasklist は正常に更新されました'
+    redirect_to @tasklist
+   else
+    flash.now[:danger] = 'Tasklist は更新されませんでした'
+    render :edit
+   end
+ end
 
-  def destroy
-    @tasklist = Tasklist.find(params[:id])
-    @tasklist.destroy
+ def destroy
+  @tasklist = Tasklist.find(params[:id])
+  @tasklist.destroy
 
-    flash[:success] = 'Tasklist は正常に削除されました'
-    redirect_to tasklists_url
-  end
+   flash[:success] = 'Tasklist は正常に削除されました'
+   redirect_to tasklists_url
+ end
   
   def tasklist_params
-    params.require(:tasklist).permit(:content)
+   params.require(:tasklist).permit(:content)
   end
   
 end
