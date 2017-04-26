b578dbe08a6904760f95ccce3e31e05f87850f32
tomoyaaida <jwaptmwt15@gmail.com>
2017-04-26 01:09:11 +0000

    CRUD tasklists

---
+++ app/controllers/tasklists_controller.rb
@@ -0,0 +1,55 @@
+class TasklistsController < ApplicationController
+ def index
+  @tasklists = Tasklist.all
+ end
+
+ def show
+  @tasklist = Tasklist.find(params[:id])
+ end
+
+ def new
+  @tasklist = Tasklist.new
+ end
+
+ def create
+  @tasklist = Tasklist.new(tasklist_params)
+
+  if @tasklist.save
+    flash[:success] = 'Tasklist が正常に投稿されました'
+    redirect_to @tasklist
+  else
+    flash.now[:danger] = 'Tasklist が投稿されませんでした'
+    render :new
+  end
+ end
+
+　def edit
+　 @tasklist = Tasklist.find(params[:id])
+　end
+
+　def update
+　 @tasklist = Tasklist.find(params[:id])
+
+    if @tasklist.update(tasklist_params)
+      flash[:success] = 'Tasklist は正常に更新されました'
+      redirect_to @tasklist
+    else
+      flash.now[:danger] = 'Tasklist は更新されませんでした'
+      render :edit
+    end
+　end
+
+  def destroy
+    @tasklist = Tasklist.find(params[:id])
+    @tasklist.destroy
+
+    flash[:success] = 'Tasklist は正常に削除されました'
+    redirect_to tasklists_url
+  end
+  
+  def tasklist_params
+    params.require(:tasklist).permit(:content)
+  end
+  
+end
+
